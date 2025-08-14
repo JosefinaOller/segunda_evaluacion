9a4104c301b4150dc8644b686e94fb871b98e100
Josefina Oller <64923768+JosefinaOller@users.noreply.github.com>
2025-08-14 15:00:43 -0300

    Inicilizar el proyecto Spring Boot

---
+++ src/test/java/com/mobydigital/evaluacion/EvaluacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobydigital.evaluacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
