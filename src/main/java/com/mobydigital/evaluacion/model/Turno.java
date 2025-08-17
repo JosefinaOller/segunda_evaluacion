97c9ee59f7b76588963fb35f124f4cbfb14768bb
Josefina Oller <64923768+JosefinaOller@users.noreply.github.com>
2025-08-17 20:30:46 -0300

    Agregar anotaciones JPA para el modelo de datos

--- src/main/java/com/mobydigital/evaluacion/model/Turno.java
+++ src/main/java/com/mobydigital/evaluacion/model/Turno.java
@@ -1,13 +1,12 @@
 package com.mobydigital.evaluacion.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Getter @Setter
 @AllArgsConstructor @NoArgsConstructor
@@ -17,6 +16,14 @@ public class Turno {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name= "paciente_id")
     private Paciente paciente;
+
+    @ManyToOne
+    @JoinColumn(name = "profesiona_id")
     private Profesional profesional;
+
+    private LocalDate fecha;
 }
