0cac3f4d6d26f734ae08161f11a2e8f7b1fb1dca
Josefina Oller <64923768+JosefinaOller@users.noreply.github.com>
2025-08-17 17:18:46 -0300

    Agregar método para buscar turnos por fecha

--- src/main/java/com/mobydigital/evaluacion/repository/ITurnoRepository.java
+++ src/main/java/com/mobydigital/evaluacion/repository/ITurnoRepository.java
@@ -4,6 +4,11 @@ import com.mobydigital.evaluacion.model.Turno;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface ITurnoRepository extends JpaRepository<Turno, Long> {
+    List<Turno> findByFecha(LocalDate fecha);
 }
