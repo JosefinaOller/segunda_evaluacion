994d26fee1b98017aafcca0ae8922765a4af65f7
Josefina Oller <64923768+JosefinaOller@users.noreply.github.com>
2025-08-17 18:47:03 -0300

    Implementar endpoint GET /turnos para listar turnos

--- src/main/java/com/mobydigital/evaluacion/controller/TurnoController.java
+++ src/main/java/com/mobydigital/evaluacion/controller/TurnoController.java
@@ -3,9 +3,12 @@ package com.mobydigital.evaluacion.controller;
 import com.mobydigital.evaluacion.model.Turno;
 import com.mobydigital.evaluacion.service.ITurnoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/turnos")
@@ -13,9 +16,16 @@ public class TurnoController {
 
     @Autowired
     private ITurnoService turnoService;
+
     @PostMapping
     public String createTurno(Turno turno){
         turnoService.saveTurno(turno);
         return "El turno fue creado correctamente";
     }
+
+    @GetMapping
+    public List<Turno> getTurnos(){
+        return turnoService.getTurnos();
+    }
+    
 }
