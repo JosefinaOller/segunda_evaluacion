6b228d04edd657670c0532e9d6ff822416a55df4
Josefina Oller <64923768+JosefinaOller@users.noreply.github.com>
2025-08-17 17:18:31 -0300

    Agregar método para buscar profesionales por especialidad

--- src/main/java/com/mobydigital/evaluacion/repository/IProfesionalRepository.java
+++ src/main/java/com/mobydigital/evaluacion/repository/IProfesionalRepository.java
@@ -4,6 +4,9 @@ import com.mobydigital.evaluacion.model.Profesional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IProfesionalRepository extends JpaRepository<Profesional, Long> {
+    List<Profesional> findByEspecialidad(String especialidad);
 }
