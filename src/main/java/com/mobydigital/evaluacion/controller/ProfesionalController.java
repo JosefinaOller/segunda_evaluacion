b3c3e58c8a301dfd5a6e90b101d58f14032a172e
Josefina Oller <64923768+JosefinaOller@users.noreply.github.com>
2025-08-17 18:42:34 -0300

    Implementar endpoint GET /profesionales?especialidad=.... para listar por especialidad

--- src/main/java/com/mobydigital/evaluacion/controller/ProfesionalController.java
+++ src/main/java/com/mobydigital/evaluacion/controller/ProfesionalController.java
@@ -3,9 +3,9 @@ package com.mobydigital.evaluacion.controller;
 import com.mobydigital.evaluacion.model.Profesional;
 import com.mobydigital.evaluacion.service.IProfesionalService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/profesionales")
@@ -18,6 +18,11 @@ public class ProfesionalController {
     public String createProfesional(Profesional profesional){
         profService.saveProfesional(profesional);
         return "El profesional fue creado correctamente";
+    }
+
+    @GetMapping
+    public List<Profesional> findByEspecialidad(@RequestParam(required = false) String especialidad){
+        return profService.findByEspecialidad(especialidad);
     }
 
 }
