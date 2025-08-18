d64e99c4459449161977fab2fd6dbe4bcdb75e6b
Josefina Oller <64923768+JosefinaOller@users.noreply.github.com>
2025-08-18 18:59:34 -0300

    Añadir @NotBlank, @Size, @Pattern y @Email en Paciente para validaciones

--- src/main/java/com/mobydigital/evaluacion/model/Paciente.java
+++ src/main/java/com/mobydigital/evaluacion/model/Paciente.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,8 +18,21 @@ public class Paciente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    @Size(min = 2, max = 30)
     private String nombre;
+
+    @NotBlank
+    @Size(min = 2, max = 30)
     private String apellido;
+
+    @NotBlank
+    @Size(min = 7, max = 8)
+    @Pattern(regexp = "\\d+") //solo números
     private String dni;
+
+    @NotBlank
+    @Email
     private String email;
 }
