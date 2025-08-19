0fce1081e7f16f61253fda3aec80eaf1eb9666b7
Josefina Oller <64923768+JosefinaOller@users.noreply.github.com>
2025-08-18 21:44:39 -0300

    Añadir funcionalidad para verificar la existencia de un turno

--- src/main/java/com/mobydigital/evaluacion/repository/ITurnoRepository.java
+++ src/main/java/com/mobydigital/evaluacion/repository/ITurnoRepository.java
@@ -1,5 +1,7 @@
 package com.mobydigital.evaluacion.repository;
 
+import com.mobydigital.evaluacion.model.Paciente;
+import com.mobydigital.evaluacion.model.Profesional;
 import com.mobydigital.evaluacion.model.Turno;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,8 +9,11 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ITurnoRepository extends JpaRepository<Turno, Long> {
     List<Turno> findByFecha(LocalDate fecha);
+
+    Optional<Turno> findByPacienteAndProfesionalAndFecha(Paciente paciente, Profesional profesional, LocalDate fecha);
 }
