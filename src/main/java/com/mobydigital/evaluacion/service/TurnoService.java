6b6b7f2a9baa23cb2eec665c7b3586189d30b39d
Josefina Oller <64923768+JosefinaOller@users.noreply.github.com>
2025-08-18 21:05:48 -0300

    Mejorar el método save para el manejo de errores

--- src/main/java/com/mobydigital/evaluacion/service/TurnoService.java
+++ src/main/java/com/mobydigital/evaluacion/service/TurnoService.java
@@ -1,34 +1,62 @@
 package com.mobydigital.evaluacion.service;
 
+import com.mobydigital.evaluacion.exception.DatoInvalidoException;
+import com.mobydigital.evaluacion.exception.RecursoNoEncontradoException;
+import com.mobydigital.evaluacion.model.Paciente;
+import com.mobydigital.evaluacion.model.Profesional;
 import com.mobydigital.evaluacion.model.Turno;
+import com.mobydigital.evaluacion.repository.IPacienteRepository;
+import com.mobydigital.evaluacion.repository.IProfesionalRepository;
 import com.mobydigital.evaluacion.repository.ITurnoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TurnoService implements ITurnoService{
 
     @Autowired
-    private ITurnoRepository turnoRepo;
+    private ITurnoRepository turnoRepository;
+
+    @Autowired
+    private IPacienteRepository pacienteRepository;
+
+    @Autowired
+    private IProfesionalRepository profesionalRepository;
     @Override
     public Turno saveTurno(Turno turno) {
-        return turnoRepo.save(turno);
+        //Validar si existe Paciente
+        Optional<Paciente> pacExistente = pacienteRepository.findById(turno.getPaciente().getId());
+        if (pacExistente.isEmpty()) {
+            throw new RecursoNoEncontradoException("El paciente con ID " + turno.getPaciente().getId() + " no existe.");
+        }
+        //Validar si existe Profesional
+        Optional<Profesional> profExistente = profesionalRepository.findById(turno.getProfesional().getId());
+        if (profExistente.isEmpty()) {
+            throw new RecursoNoEncontradoException("El profesional con ID " + turno.getProfesional().getId() + " no existe.");
+        }
+        //Validar que no haya duplicados
+        if (turnoRepository.findByPacienteAndProfesionalAndFecha(turno.getPaciente(),turno.getProfesional(),turno.getFecha()).isPresent()) {
+            throw new DatoInvalidoException("Ya existe un turno para el mismo paciente, profesional y fecha. ");
+        }
+
+        return turnoRepository.save(turno);
     }
     @Override
     public List<Turno> getTurnos() {
-        return turnoRepo.findAll();
+        return turnoRepository.findAll();
     }
 
     @Override
     public List<Turno> findByFecha(LocalDate fecha) {
-        return turnoRepo.findByFecha(fecha);
+        return turnoRepository.findByFecha(fecha);
     }
 
     @Override
     public void deleteTurno(Long id) {
-        turnoRepo.deleteById(id);
+        turnoRepository.deleteById(id);
     }
 }
